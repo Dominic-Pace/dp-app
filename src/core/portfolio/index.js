86464430f1223e6ccd2f23a6e9e368aaac35e8c8
Dominic-Pace <dpace@nhpace.net>
2018-03-01 19:14:03 -0500

    Added to portfolio

--- src/core/portfolio/index.js
+++ src/core/portfolio/index.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import { Col, Row } from 'react-bootstrap'
 
+import SquadPic from '../../assets/img/squad.png'
+import WirlixPic from '../../assets/img/wirlix.png'
+
 import Header from '../../components/header'
 import PortfolioCard from '../../components/portfolio-card'
 import Scrollable from '../../components/scrollable'
@@ -14,6 +17,34 @@ const PortfolioContent = () => (
       height={75}
     >
       <Row className='portfolio-items'>
+        <Col md={2} />
+        <Col
+          className='justify-center'
+          md={4}
+        >
+          <PortfolioCard
+            headerImg='http://www.publicdomainpictures.net/pictures/230000/velka/healthcare.jpg'
+            cardTitle='Caféwell Coaching'
+          />
+          <PortfolioCard
+            headerImg='https://cdn-images-1.medium.com/max/1600/1*6Rz95aDqIK09XLa6NppGaw.png'
+            cardTitle='Caféwell Central'
+          />
+        </Col>
+        <Col
+          className='justify-center'
+          md={4}
+        >
+          <PortfolioCard
+            headerImg={WirlixPic}
+            cardTitle='Wirlix Mobile'
+          />
+          <PortfolioCard
+            headerImg={SquadPic}
+            cardTitle='Squad Mobile App'
+          />
+        </Col>
+        <Col md={2} />
       </Row>
     </Scrollable>
   </Col>
